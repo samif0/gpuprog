662a5f8f4e0d7fbcf3c7aabf1732f25214fd0b9c
Sami Fawcett <samifawcett.nyc@gmail.com>
2025-03-18 01:10:10 +0000

    first gpu kernel

--- main.cu
+++ main.cu
@@ -1,5 +1,26 @@
 #include <iostream>
 #include <stdlib.h>
+#include <cstdio>
+
+__global__ void whoami(void) {
+    int block_id = 
+        blockIdx.x +
+        blockIdx.y * gridDim.x +
+        blockIdx.x * gridDim.x * gridDim.y;
+
+    int block_offset = 
+        block_id * 
+        blockDim.x * blockDim.y * blockDim.z;
+
+    int thread_offset =
+        threadIdx.x +
+        threadIdx.y * blockDim.x +
+        threadIdx.z * blockDim.x * blockDim.y;
+
+    int id = block_offset + thread_offset;
+
+    printf("%04d\n", id); 
+}
 
 int main(int argc, char const *argv[])
 {  
@@ -12,5 +33,14 @@ int main(int argc, char const *argv[])
     std::cout << "bl/g: " << blocks_per_grid << std::endl;
     std::cout << "th/g: " << threads_per_block << std::endl;
     std::cout << "total threads: " << blocks_per_grid * threads_per_block << std::endl;
+
+
+    dim3 cuBPG(b_x, b_y, b_z);
+    dim3 cuTPB(t_x, t_y, t_z);
+
+
+    whoami<<<cuBPG, cuTPB>>>();
+    cudaDeviceSynchronize();
+
     return EXIT_SUCCESS;
 }
